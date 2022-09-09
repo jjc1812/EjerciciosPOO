e4fbdedb52d14413ca0873deef7ce59eae9e126a
jjc1812 <josejcronel98@gmail.com>
2022-09-09 20:15:25 -0300

    encapsulamiento

--- main/src/main/java/com/objetos/Auto.java
+++ main/src/main/java/com/objetos/Auto.java
@@ -1,25 +1,28 @@
 package com.objetos;
 
-public class Auto {
+class Auto {
     int puertas;
     String marca = "ford";
-    boolean cerokm = false;
+    private boolean cerokm = false;
     String fechaFabricacion = "23/04/1970";
 
-    int kilometraje() {
+    private int kilometraje() {
         return 10000;
     }
 
-    void modelo() {
+    public void modelo() {
         System.out.println("fiesta");
         System.out.println(marca);
+        System.out.println(cerokm);
+        System.out.println(kilometraje());
     }
 }
 
 class DemoAuto {
-    public static void main(String[] args) {
+    public static void main( String[] args ){
         Auto a = new Auto();
+        System.out.println(a.puertas);
+        System.out.println(a.fechaFabricacion);
         a.modelo();
-        System.out.println(a.kilometraje());
     }
 }
