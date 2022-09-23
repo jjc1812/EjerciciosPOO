e4d44e5b2ac7f33519e7173defd8bfd69f1c80af
jjc1812 <josejcronel98@gmail.com>
2022-09-23 19:05:39 -0300

    Herecncia y polimorfismo

--- main/src/main/java/com/objetos/Auto.java
+++ main/src/main/java/com/objetos/Auto.java
@@ -1,19 +1,29 @@
 package com.objetos;
 
-class Auto {
+class Auto extends Vehiculos{
     private int puertas;
     private String marca;
     private boolean cerokm;
     private String fechaFabricacion;
 
     public Auto() {
+        super();
         this.puertas = 5;
         this.marca = "ford";
         this.cerokm = false;
         this.fechaFabricacion = "17/08/1990";
     }
 
-    public Auto(int newPuerta, String newMarca, Boolean newCeroKM, String newFechaFabricacion){
+    public Auto(
+            int newPuerta, 
+            String newMarca, 
+            Boolean newCeroKM, 
+            String newFechaFabricacion, 
+            String newMotor, 
+            String newColor, 
+            int newPeso
+        ){
+        super(newMotor, newColor, newPeso);
         this.puertas = newPuerta;
         this.marca = newMarca;
         this.cerokm = newCeroKM;
@@ -49,14 +59,15 @@ class Auto {
     }
 
     public String toString(){
-        return this.puertas + " " + this.marca + " " + this.cerokm + " " + this.fechaFabricacion;
+        return this.puertas + " " + this.marca + " " + this.cerokm + " " + this.fechaFabricacion + " " + this.motor + " " + this.color + " " + this.peso;
     }
 }
 
 class DemoAuto {
     public static void main( String[] args ){
         Auto a = new Auto();
-        Auto a2 = new Auto(3, "fiat", true, "20/05/2022");
+        Auto a2 = new Auto(3, "fiat", true, "20/05/2022", "1.6", "gris", 900);
+        Vehiculos a3 = new Auto(4, "ferrari", false, "10/07/1980", "V6", "rojo", 700);
         System.out.println(a.toString());
         a.modelo();
         a.setPuertas(4);
@@ -65,5 +76,7 @@ class DemoAuto {
         a2.setMarca("Mercedes");
         System.out.println(a2.toString());
         a2.modelo();
+        System.out.println(a2.pesoNeto(10));
+        System.out.println(a3.toString());
     }
 }
---
+++ main/src/main/java/com/objetos/Vehiculos.java
@@ -0,0 +1,23 @@
+package com.objetos;
+
+public class Vehiculos {
+    protected String motor;
+    protected String color;
+    protected int peso;
+
+    public Vehiculos(){
+        this.motor = "V8";
+        this.color = "negro";
+        this.peso = 1000;
+    }
+
+    public Vehiculos(String newMotor, String newColor, int newPeso){
+        this.motor = newMotor;
+        this.color = newColor;
+        this.peso = newPeso;
+    }
+
+    public int pesoNeto(int pesoCarga){
+        return pesoCarga+peso;
+    }
+}
