81cfffaa11cb6614dfddafeeb2c2b3aa58c6dfa9
jjc1812 <josejcoronel98@gmail.com>
2022-10-28 20:14:27 -0300

    Conexion a la base de datos

---
+++ main/src/main/java/com/objetos/DemoBD.java
@@ -0,0 +1,26 @@
+package com.objetos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DemoBD {
+
+    static final String BD_Conexion = "jdbc:mysql://localhost:3306/";
+    static final String Usuario_BD = "root";
+    static final String Contrasena_BD = "root";
+
+    public static void main( String[] args ){
+
+        try(Connection con = DriverManager.getConnection(BD_Conexion, Usuario_BD, Contrasena_BD);
+        Statement stmt = con.createStatement()){
+            String query = "INSERT INTO POO.autos (puertas,marca,ceroKM,fecha_fabricacion) VALUES (3,'mercedes',0,'2004-07-07');";
+            stmt.executeUpdate(query);
+            System.out.println("Persistio en base de datos.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
