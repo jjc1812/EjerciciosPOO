ed34d2df7f89160de8107caebf870e59c8736883
jjc1812 <josejcoronel98@gmail.com>
2022-11-04 18:59:00 -0300

    select table

--- main/src/main/java/com/objetos/DemoBD.java
+++ main/src/main/java/com/objetos/DemoBD.java
@@ -1,7 +1,9 @@
 package com.objetos;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -13,12 +15,35 @@ public class DemoBD {
 
     public static void main( String[] args ){
 
+        int puertas = 5;
+        String marca = "ford";
+        int ceroKM = 1;
+        String fechaFabricacion = "2022-07-07";
+
         try(Connection con = DriverManager.getConnection(BD_Conexion, Usuario_BD, Contrasena_BD);
         Statement stmt = con.createStatement()){
-            String query = "INSERT INTO POO.autos (puertas,marca,ceroKM,fecha_fabricacion) VALUES (3,'mercedes',0,'2004-07-07');";
+            String query = "INSERT INTO POO.autos (puertas,marca,ceroKM,fecha_fabricacion) VALUES (" + puertas + ",'" + marca + "'," + ceroKM + ",'" + fechaFabricacion + "');";
             stmt.executeUpdate(query);
-            // stmt.executeQuery(query); caso de uso con CREATE table
+
             System.out.println("Persistio en base de datos.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        try(Connection con = DriverManager.getConnection(BD_Conexion, Usuario_BD, Contrasena_BD);
+        Statement stmt = con.createStatement()){
+            String query = "select * from poo.autos a ;";
+            ResultSet result = stmt.executeQuery(query);
+            while(result.next()){
+                int puertaBD = result.getInt("puertas");
+                String marcaBD = result.getString("marca");
+                Boolean ceroKMBD = result.getBoolean("ceroKM");
+                Date fechaFabricacionBD = result.getDate("fecha_fabricacion");
+                System.out.println("puertas: " + puertaBD +
+                " marca: " + marcaBD +
+                " ceroKM: " + ceroKMBD +
+                " fechaFabricacion: " + fechaFabricacionBD);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
